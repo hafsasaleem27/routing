8f72410025f81f1067d139df1caa30a069169cec
Hafsa Saleem <hafsa.saleem27@gmail.com>
2023-03-26 21:52:17 +0500

    added Link component in place of anchor tag to avoid page refresh

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Homepage() {
-    return <h1>My Homepage</h1>;
-};
+  return (
+    <>
+      <h1>My Homepage</h1>
+      <p>Go to <Link to="/products">the list of products</Link>.</p>
+    </>
+  );
+}
 
 export default Homepage;
